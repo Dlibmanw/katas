78e3967084ff529bae7c7d878ac888e823250e5f
Dafna Libman <dafnalibman@gmail.com>
2020-05-11 12:19:10 +0100

    Add a create_string method

--- take_the_derivative/lib/take_the_derivative.rb
+++ take_the_derivative/lib/take_the_derivative.rb
@@ -1,10 +1,21 @@
 class Kata1
+  attr_reader :numbers
+  def initialize
+    @numbers = []
+  end
+
   def multiply(num_1, num_2)
     result = num_1 * num_2
-    return result  
+    @numbers.push(result)  
   end
 
   def substract(num_2)
-    num_2 - 1
+    exponent = num_2 - 1
+    @numbers.push(exponent)
   end
+
+  def create_string
+    return "#{@numbers[0]}x^#{@numbers[1]}"
+  end
+
 end
--- take_the_derivative/spec/take_the_derivative_spec.rb
+++ take_the_derivative/spec/take_the_derivative_spec.rb
@@ -4,13 +4,22 @@ describe Kata1 do
     it 'multiply 2 given number and output the result' do
       num_1 = 7 
       num_2 = 8
-      expect(subject.multiply(num_1, num_2)).to eq 56
+      expect(subject.multiply(num_1, num_2)).to eq [56]
     end
   end
   describe '#substract' do
     it 'substract 1 from the second numer' do
       num_2 = 8
-      expect(subject.substract(num_2)).to  eq 7
+      expect(subject.substract(num_2)).to  eq [7]
     end
   end
+
+  describe '#create_string' do 
+    it 'take 2 numbers and ' do
+      subject.multiply(5, 6)
+      subject.substract(6)
+      expect(subject.create_string).to eq '30x^5'
+    end
+  end
+
 end
