121b450fb8d316f5550e0213aab52188e14d7a4d
Elena <elena.beccaro@gmail.com>
2020-05-11 11:39:47 +0100

    passed second test

--- take_the_derivative/lib/take_the_derivative.rb
+++ take_the_derivative/lib/take_the_derivative.rb
@@ -1,6 +1,10 @@
 class Kata1
   def multiply(num_1, num_2)
     result = num_1 * num_2
-    return result
+    return result  
+  end
+
+  def substract(num_2)
+    num_2 - 1
   end
 end
--- take_the_derivative/spec/take_the_derivative_spec.rb
+++ take_the_derivative/spec/take_the_derivative_spec.rb
@@ -4,7 +4,13 @@ describe Kata1 do
     it 'multiply 2 given number and output the result' do
       num_1 = 7 
       num_2 = 8
-      expect(subject.multiply(7, 8)).to eq 56
+      expect(subject.multiply(num_1, num_2)).to eq 56
+    end
+  end
+  describe '#substract' do
+    it 'substract 1 from the second numer' do
+      num_2 = 8
+      expect(subject.substract(num_2)).to  eq 7
     end
   end
 end
