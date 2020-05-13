d5bb5ccc16e1f9f494808e057b94a77de6dc3ae9
Elena <elena.beccaro@gmail.com>
2020-05-13 13:43:56 +0100

    Second test passing

--- ATM/lib/atm.rb
+++ ATM/lib/atm.rb
@@ -1,5 +1,15 @@
 def solve(amount)
-  if amount % 500 == 0
-    return 1 
+
+  count = 0
+
+  until amount == 0 
+    count += amount/500 
+      amount = amount % 500 
+    count += amount/50 
+      amount = amount % 50 
   end
+
+  return count
+    
+
 end
--- ATM/spec/atm_spec.rb
+++ ATM/spec/atm_spec.rb
@@ -4,4 +4,8 @@ describe 'atm' do
   it 'returns 1 when the input is an amount of 500' do
     expect(solve(500)).to eq 1
   end
+
+  it 'returns 2 when the input is an amout os 550' do
+    expect(solve(550)).to eq 2
+  end
 end
