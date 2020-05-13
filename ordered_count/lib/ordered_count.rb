47bc01ecbaeec53d5f40f41e025ca4c61511ed69
Elena <elena.beccaro@gmail.com>
2020-05-13 10:19:47 +0100

    test passing

--- ordered_count/lib/ordered_count.rb
+++ ordered_count/lib/ordered_count.rb
@@ -1,3 +1,3 @@
 def ordered_count(str)
-    str.scan /\w/
+    str.chars.uniq.map{|i| [i, str.count(i)]}
 end
--- ordered_count/spec/ordered_count_spec.rb
+++ ordered_count/spec/ordered_count_spec.rb
@@ -2,15 +2,15 @@ require 'ordered_count'
 
 describe '#ordered_count' do
     it 'check if there is an A' do
-        expect(ordered_count('a')).to eq ['a', 1]
+        expect(ordered_count('a')).to eq [['a', 1]]
     end
 
     it 'returns ["a", 2] when the input is "aa" ' do
-        expect(ordered_count('aa')).to eq ['a', 2]
+        expect(ordered_count('aa')).to eq [['a', 2]]
     end
 
     it 'return [["a", 1] , ["b", 1]] when the imput is "ab" ' do
         expect(ordered_count('ab')).to eq [["a", 1], ["b", 1]]
     end
 
-endgit push
+end
