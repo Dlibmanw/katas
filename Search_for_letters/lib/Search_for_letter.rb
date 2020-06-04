a9b6057c3b2115cd9832d1e38c668b26cb216650
Dafna Libman <dafnalibman@gmail.com>
2020-06-04 13:12:56 +0100

    Add a while loop and letters hash, use include to check if input contains letters compatible to current index

--- Search_for_letters/lib/Search_for_letter.rb
+++ Search_for_letters/lib/Search_for_letter.rb
@@ -1,4 +1,16 @@
-def change1(a) 
-    arr = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
-    arr.join
+def change1(string)
+    string = string 
+    letters = { 1 => 'a', 2 => 'b', 3 => 'c', 4 => 'd', 5 =>'e', 6 => 'f', 7 =>'g', 8 =>'h', 9 => 'i', 10 => 'j', 11 => 'k', 12 => 'l', 13 => 'm',
+       14 => 'n', 15 =>'o', 16 => 'p', 17 => 'q', 18 => 'r', 19 => 's', 20 => 't', 21 =>'u',  22 =>'v', 23  =>'w', 24 => 'x', 25 =>'y', 26 =>'z' }
+    i = 0
+    output = []
+    while i < 26 do
+      if string.include? "#{letters.values[i]}"
+        output.push('1')
+      else 
+        output.push('0')
+      end 
+      i += 1
+    end
+    return output.join
 end
--- Search_for_letters/spec/Search_for_letter_spec.rb
+++ Search_for_letters/spec/Search_for_letter_spec.rb
@@ -2,7 +2,13 @@ require 'Search_for_letter'
 
 describe '#change1' do
     it 'print 000000000000000000000000 when in the imput there are no letters' do
-        letters = '&'
-        expect(change1(letters)).to eq '00000000000000000000000000'
+        string = '&'
+        expect(change1(string)).to eq '00000000000000000000000000'
+    end
+
+    it 'print 10000000000000000000000000 when the input beggins with a' do
+      string = 'a'
+      expect(change1(string)).to eq '10000000000000000000000000'
     end
 end
+
