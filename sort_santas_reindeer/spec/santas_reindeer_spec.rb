4ba44417d7e52a994d3c3a218c4c447b4c3b57be
Elena <elena.beccaro@gmail.com>
2020-06-05 11:56:34 +0100

    test passing

--- sort_santas_reindeer/spec/santas_reindeer_spec.rb
+++ sort_santas_reindeer/spec/santas_reindeer_spec.rb
@@ -5,6 +5,4 @@ describe 'sort_reindeer' do
   it 'returns a list sorted by second name' do
     expect(sort_reindeer(["Prancer Chua", "Dasher Tonoyan"])).to eq ["Prancer Chua", "Dasher Tonoyan"]
   end 
-
-  
 end
