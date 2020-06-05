cdc33c4a126e8a809301491a533d0052ad908f01
Dafna Libman <dafnalibman@gmail.com>
2020-06-05 12:31:25 +0100

    Update sorting based on last name (remove reverse method)

--- sort_santas_reindeer/lib/santas_reindeer.rb
+++ sort_santas_reindeer/lib/santas_reindeer.rb
@@ -2,11 +2,13 @@ def sort_reindeer reindeer_names
   reindeer_names = reindeer_names 
   splited = []
   reindeer_names.each do |name|
-    splited.push(name.split(" ").reverse)
+    splited.push(name.split(" "))
   end
+
   output = []
-  splited.sort.each do |name|
-    output.push(name.reverse.join(' '))
+  
+  (splited.sort_by {|name| name[1]}).each do |name|
+    output.push(name.join(' '))
   end
   return output
 end
