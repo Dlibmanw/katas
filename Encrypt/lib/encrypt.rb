389ba892d002ad1dccce4deb5eb7aab1908058e2
Dafna Libman <dafnalibman@gmail.com>
2020-06-08 13:36:39 +0100

    Add a hash for ascii code, replace first letter with the hash value for that letter.

--- Encrypt/lib/encrypt.rb
+++ Encrypt/lib/encrypt.rb
@@ -1,11 +1,14 @@
 
 def decipher_this(string)
     arr = []
-    ascii = { 'A': '65', 'H': '72'}
+    ascii = { 'A' => '65', 'B' => '66', 'H' => '72'}
     arr<<string.split("")
 
-    #arr[0].first()
-    #return ascii[:'H']
-    return arr[0].map{ |x| x == 'H'  ? ascii[:'H']  : x }.join
-
+    return arr[0].map{ |x| 
+        if ascii.include? (x) 
+          ascii[x]
+        else
+          x
+        end
+      }.join
 end
--- Encrypt/spec/encrypt_spec.rb
+++ Encrypt/spec/encrypt_spec.rb
@@ -4,4 +4,9 @@ describe '#decipher_this' do
     it 'return 72ello if the input Hello' do
         expect(decipher_this('Hello')).to eq '72ello'
     end
+
+    it 'return 72ello if the input Hello' do
+      expect(decipher_this('Bye')).to eq '66ye'
+    end
+
 end
