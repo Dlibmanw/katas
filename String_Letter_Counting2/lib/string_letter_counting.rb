33f427e69ea7d63891cfefc078cffb14b91be2ff
Elena <elena.beccaro@gmail.com>
2020-06-15 12:02:16 +0100

    add test for integer, and empty array

--- String_Letter_Counting2/lib/string_letter_counting.rb
+++ String_Letter_Counting2/lib/string_letter_counting.rb
@@ -2,9 +2,14 @@ def string_letter_count(string)
   string = string.split('')
   output = []
   string.each do |letter|
-    if output.count(letter) == 0
+    if 
+      letter.count("a-zA-Z") > 0
+      if output.count(letter) == 0
       output << string.count(letter)
       output << letter
+      end
+    else
+      return output = ""
     end
   end
   return output.join()
--- String_Letter_Counting2/spec/string_letter_counting_spec.rb
+++ String_Letter_Counting2/spec/string_letter_counting_spec.rb
@@ -4,4 +4,23 @@ describe '' do
   it 'returns 3a when the input is "aaa"' do
     expect(string_letter_count('aaa')).to eq '3a'
   end
+
+  it 'returns 3a when the input is "abc"' do
+    expect(string_letter_count('abc')).to eq '1a1b1c'
+  end
+
+  it 'returns "" when the input is ""' do
+    expect(string_letter_count('')).to eq ''
+  end
+
+  it 'returns "" when the input is "555"' do
+    expect(string_letter_count('555')).to eq ''
+  end
+
+  it 'returns "" when the input is "/."' do
+    expect(string_letter_count('/.')).to eq ''
+  end
+   #it 'returns 1e1h2l1o when the input is "hello"' do
+   # expect(string_letter_count('hello')).to eq '1e1h2l1o'
+  #end
 end
