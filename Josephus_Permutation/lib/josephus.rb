6d022d0e8dd221ce344ec14e0b380664770b8a28
Elena <elena.beccaro@gmail.com>
2020-06-17 14:44:09 +0100

    add .rotate

--- Josephus_Permutation/lib/josephus.rb
+++ Josephus_Permutation/lib/josephus.rb
@@ -1,21 +1,10 @@
 def josephus(items,k)
   items = items
-  i = 1
-  new_array = []
-  result = []
-  unless items.length == 0
-    items.each do |num|
-      if (i < items.length) && (i % 3 != 0)
-        new_array << num
-        i += 1
-      elsif (i < items.length) && (i % 3 == 0)
-        result << items.slice!(i+1)
-        i += 1
-      else
-        items = new_array
-        i = 1
-      end
-    end
+  k = 2
+  arr = []
+  unless items.length == 0 
+    arr << items.rotate(k).slice!(0)
   end
-  return result
+  return arr
+ 
 end
--- Josephus_Permutation/spec/josephus_spec.rb
+++ Josephus_Permutation/spec/josephus_spec.rb
@@ -4,4 +4,7 @@ describe 'josephus' do
   it 'returns [3, 1, 2] when the input is [1, 2, 3]' do
     expect(josephus([1, 2, 3], 3)).to eq [3, 1, 2]
   end
+  it 'returns [3, 1, 2] when the input is [1, 2, 3]' do
+    expect(josephus([1, 2, 3, 4, 5, 6, 7], 3)).to eq [1,2,3,4,5,6,7]
+  end
 end
