83363f72c7d60db27da5e80d9098dfc55fdb228d
Dafna Libman <dafnalibman@gmail.com>
2020-06-18 16:58:27 +0100

    Update method to check if a number has been removed

--- Josephus_Permutation/lib/josephus.rb
+++ Josephus_Permutation/lib/josephus.rb
@@ -1,20 +1,19 @@
 def josephus(items,k)
   items = items
-  l = items.length
   k = k
   result = []
   i = 1 
-  while result.length != l 
+  while result.length != items.length 
     for num in items do
-      if i % k != 0 
-        i += 1
-      else i % k == 0 
-        result << items.slice(items.find_index(num))
-        # items.delete(num)
-        i = 1
+      if !result.include?(num)
+        if i % k != 0 
+          i += 1
+        elsif i % k == 0 
+          result << items.slice(items.find_index(num))
+          i = 1
+        end
       end
     end
   end
   return result
- 
 end
