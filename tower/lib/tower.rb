2846d74bd44c53efb02a7ca7f863b4e91eef91dd
Elena <elena.beccaro@gmail.com>
2020-06-19 11:46:42 +0100

    second test passing

--- tower/lib/tower.rb
+++ tower/lib/tower.rb
@@ -1,9 +1,15 @@
 def towerBuilder(n_floors)
-    n_floors = n_floors
-    # i = 0
-    for i in 0..n_floors 
-        return ['*']
-
+    arr = []
+    star = '*'
+    i = 0
+    for num in 1..n_floors do
+        if num == 1
+            arr << star 
+        else 
+            i = i + 1 
+            arr << (star * num) + (star * i ) 
+        end
     end
+    return arr
 end
 
--- tower/spec/tower_spec.rb
+++ tower/spec/tower_spec.rb
@@ -4,4 +4,8 @@ describe 'towerBuilder' do
     it 'it return * if the input is 1' do
         expect(towerBuilder(1)).to eq ['*']
     end
+
+    it 'it return "*", "***", "*****" if the input is 3' do
+        expect(towerBuilder(3)).to eq ['*', '***', '*****']
+    end
 end
