837c060849177836335697eed311a66ae087d318
Elena <elena.beccaro@gmail.com>
2020-06-23 12:42:24 +0100

    add edge case test

--- Number_Zoo_Patrol/lib/number_zoo.rb
+++ Number_Zoo_Patrol/lib/number_zoo.rb
@@ -1,13 +1,18 @@
 def find_missing_number(numbers)
     missing_num = []
-    if numbers.length == numbers[-1] 
+
+    if numbers.sort.length == numbers.sort[-1] 
       missing_num << (numbers.length + 1)
+
+    elsif numbers.empty?
+        missing_num << 1
+
     else
       for num in 1..numbers.length do
           if !numbers.include?(num)
               missing_num << num
           end      
-      end
+        end
     end
     return missing_num[0]
-  end
+end
--- Number_Zoo_Patrol/spec/number_zoo_spec.rb
+++ Number_Zoo_Patrol/spec/number_zoo_spec.rb
@@ -8,4 +8,20 @@ describe 'find_missing_number' do
     it 'it return 4 when the input is [1, 2, 3]' do
         expect(find_missing_number([1, 2, 3])).to eq 4
     end
+
+    it 'it return 1 when the input is [2, 3, 4]' do
+        expect(find_missing_number([2, 3, 4])).to eq 1
+    end
+
+    it 'it return 1 when the input is [2]' do
+        expect(find_missing_number([2])).to eq 1
+    end
+    
+    it 'it return 1 when the input is []' do
+        expect(find_missing_number([])).to eq 1
+    end
+
+    it 'it return 4 when the input is [3, 2, 1]' do
+        expect(find_missing_number([3, 2, 1])).to eq 4
+    end
 end
