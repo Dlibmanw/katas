082bbf0c95f43d87828eea1f86d53eca6b23ef45
Dafna Libman <dafnalibman@gmail.com>
2020-06-23 12:19:56 +0100

    Update method to check if the missing number is the last number

--- Number_Zoo_Patrol/lib/number_zoo.rb
+++ Number_Zoo_Patrol/lib/number_zoo.rb
@@ -1,8 +1,13 @@
 def find_missing_number(numbers)
-
-    for num in 1..numbers.length do
-        if !numbers.include?(num)
-            return num
-        end
+    missing_num = []
+    if numbers.length == numbers[-1] 
+      missing_num << (numbers.length + 1)
+    else
+      for num in 1..numbers.length do
+          if !numbers.include?(num)
+              missing_num << num
+          end      
+      end
     end
+    return missing_num[0]
   end
