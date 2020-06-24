d90f4f7fd93bd2044d876c08b8526fa49bfa0520
Dafna Libman <dafnalibman@gmail.com>
2020-06-24 14:13:21 +0100

    Update the method to check if the letter appearse in the other string both in up and lowercase

--- Play_with_two_Strings/lib/two_strings.rb
+++ Play_with_two_Strings/lib/two_strings.rb
@@ -2,16 +2,32 @@ def work_on_strings(x, y)
   occurance_x = y.count(x)
   occurance_y = x.count(y)
   output = []
-  if occurance_x.odd? && occurance_y.odd?
-    output << x.upcase + y.upcase
-  elsif occurance_x.even? && occurance_y.odd?
-    output << x + y.upcase
-  elsif occurance_x.odd? && occurance_y.even?
-    output << x.upcase + y
-  else
-    output << x + y
-  end
+
+  x.split('').each {|letter|
+    if (y.count(letter.upcase) + y.count(letter.downcase)).odd? 
+      output << letter.upcase
+    else
+      output << letter
+    end
+  }
+  y.split('').each {|letter|
+    if (x.count(letter.upcase) + x.count(letter.downcase)).odd? 
+      output << letter.upcase
+    else
+      output << letter
+    end
+  }
   return output.join
+  # if occurance_x.odd? && occurance_y.odd?
+  #   output << x.upcase + y.upcase
+  # elsif occurance_x.even? && occurance_y.odd?
+  #   output << x + y.upcase
+  # elsif occurance_x.odd? && occurance_y.even?
+  #   output << x.upcase + y
+  # else
+  #   output << x + y
+  # end
+  # return output.join
   
 end
 
