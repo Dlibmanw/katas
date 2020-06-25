a9dd887a832d6f574f230a09fd5ab0b79d41f4ef
Dafna Libman <dafnalibman@gmail.com>
2020-06-25 13:08:55 +0100

    Refactor

--- Play_with_two_Strings/lib/two_strings.rb
+++ Play_with_two_Strings/lib/two_strings.rb
@@ -1,23 +1,6 @@
 def work_on_strings(x, y)
-  occurance_x = y.count(x)
-  occurance_y = x.count(y)
   output = []
-
-  x.split('').each {|letter|
-    if (y.count(letter.upcase) + y.count(letter.downcase)).odd? 
-      output << letter.swapcase
-    else
-      output << letter
-    end
-  }
-  y.split('').each {|letter|
-    if (x.count(letter.upcase) + x.count(letter.downcase)).odd? 
-      output << letter.swapcase
-    else
-      output << letter
-    end
-  }
-return output.join
-  
+  x.split('').each {|letter|(y.count(letter.upcase) + y.count(letter.downcase)).odd?? output << letter.swapcase : output << letter}
+  y.split('').each {|letter|(x.count(letter.upcase) + x.count(letter.downcase)).odd?? output << letter.swapcase: output << letter}
+  return output.join
 end
-
